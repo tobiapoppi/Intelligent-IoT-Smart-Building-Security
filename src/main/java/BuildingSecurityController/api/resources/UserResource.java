16b3776cb3ffd8dda230837b03888daacc586c02
Tobia <tobia.poppi@virgilio.it>
2020-12-06 13:26:19 +0100

    Methods handlers for user resource are completed

--- src/main/java/BuildingSecurityController/api/resources/UserResource.java
+++ src/main/java/BuildingSecurityController/api/resources/UserResource.java
@@ -1,6 +1,12 @@
 package BuildingSecurityController.api.resources;
 
 
+import BuildingSecurityController.api.auth.User;
+import BuildingSecurityController.api.data_transfer_object.PolicyCreationRequest;
+import BuildingSecurityController.api.data_transfer_object.PolicyUpdateRequest;
+import BuildingSecurityController.api.data_transfer_object.UserCreationRequest;
+import BuildingSecurityController.api.data_transfer_object.UserUpdateRequest;
+import BuildingSecurityController.api.exception.IInventoryDataManagerConflict;
 import BuildingSecurityController.api.model.PolicyDescriptor;
 import BuildingSecurityController.api.model.UserDescriptor;
 import BuildingSecurityController.api.services.OperatorAppConfig;
@@ -12,15 +18,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.security.RolesAllowed;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @Path("/api/iot/inventory/user")
 @Api("IoT User Inventory Endpoint")
@@ -42,8 +48,8 @@ public class UserResource {
     @Path("/")
     @Timed
     @Produces(MediaType.APPLICATION_JSON)
-    @ApiOperation(value = "Get all registered Users")
-    public Response GetUser(@Context ContainerRequestContext requestContext){
+    @ApiOperation(value = "Get all registered Users' usernames")
+    public Response GetUserList(@Context ContainerRequestContext requestContext){
 
         try{
             logger.info("Loading all stored IoT Inventory Policies.");
@@ -60,6 +66,136 @@ public class UserResource {
         } catch(Exception e){
             e.printStackTrace();
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).type(MediaType.APPLICATION_JSON_TYPE).entity(new ErrorMessage(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(), "Internal Server Error!")).build();
+        }
+    }
+
+    @RolesAllowed("ADMIN")
+    @GET
+    @Path("/{username}")
+    @Timed
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value="Get a Single User")
+    public Response getUser(@Context ContainerRequestContext requestContext,
+                                @PathParam("username") String username) {
+
+        try {
+
+            logger.info("Loading User Info for id: {}", username);
+
+            //Check the request
+            if(username == null)
+                return Response.status(Response.Status.BAD_REQUEST).type(MediaType.APPLICATION_JSON_TYPE).entity(new ErrorMessage(Response.Status.BAD_REQUEST.getStatusCode(),"Invalid Username Provided !")).build();
+
+            Optional<UserDescriptor> userDescriptor = this.conf.getInventoryDataManager().getUser(username);
+
+            if(!userDescriptor.isPresent())
+                return Response.status(Response.Status.NOT_FOUND).type(MediaType.APPLICATION_JSON_TYPE).entity(new ErrorMessage(Response.Status.NOT_FOUND.getStatusCode(),"User Not Found !")).build();
+
+            return Response.ok(userDescriptor.get()).build();
+
+        } catch (Exception e){
+            e.printStackTrace();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).type(MediaType.APPLICATION_JSON_TYPE).entity(new ErrorMessage(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(),"Internal Server Error !")).build();
+        }
+    }
+
+    @RolesAllowed("ADMIN")
+    @POST
+    @Path("/")
+    @Timed
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @ApiOperation(value="Create a new User")
+    public Response createLocation(@Context ContainerRequestContext req,
+                                   @Context UriInfo uriInfo,
+                                   UserCreationRequest userCreationRequest) {
+
+        try {
+
+            logger.info("Incoming User Creation Request: {}", userCreationRequest);
+
+            //Check the request
+            if(userCreationRequest == null)
+                return Response.status(Response.Status.BAD_REQUEST).type(MediaType.APPLICATION_JSON_TYPE).entity(new ErrorMessage(Response.Status.BAD_REQUEST.getStatusCode(),"Invalid request payload")).build();
+
+            UserDescriptor newUserDescriptor = (UserDescriptor) userCreationRequest;
+
+            newUserDescriptor = this.conf.getInventoryDataManager().createNewUser(newUserDescriptor);
+
+            return Response.created(new URI(String.format("%s/%s",uriInfo.getAbsolutePath(),newUserDescriptor.getUsername()))).build();
+
+        } catch (IInventoryDataManagerConflict e){
+            return Response.status(Response.Status.CONFLICT).type(MediaType.APPLICATION_JSON_TYPE).entity(new ErrorMessage(Response.Status.CONFLICT.getStatusCode(),"User already exists !")).build();
+        } catch (Exception e){
+            e.printStackTrace();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).type(MediaType.APPLICATION_JSON_TYPE).entity(new ErrorMessage(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(),"Internal Server Error !")).build();
+        }
+    }
+
+    @RolesAllowed("ADMIN")
+    @PUT
+    @Path("/{username}")
+    @Timed
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @ApiOperation(value="Update an existing User")
+    public Response updateUser(@Context ContainerRequestContext req,
+                                   @Context UriInfo uriInfo,
+                                   @PathParam("username") String username,
+                                   UserUpdateRequest userUpdateRequest) {
+
+        try {
+
+            logger.info("Incoming User ({}) Update Request: {}", username, userUpdateRequest);
+
+            //Check if the request is valid, the id must be the same in the path and in the json request payload
+            if(userUpdateRequest == null || !userUpdateRequest.getUsername().equals(username))
+                return Response.status(Response.Status.BAD_REQUEST).type(MediaType.APPLICATION_JSON_TYPE).entity(new ErrorMessage(Response.Status.BAD_REQUEST.getStatusCode(),"Invalid request ! Check Username")).build();
+
+            //Check if the device is available and correctly registered otherwise a 404 response will be sent to the client
+            if(!this.conf.getInventoryDataManager().getUser(username).isPresent())
+                return Response.status(Response.Status.NOT_FOUND).type(MediaType.APPLICATION_JSON_TYPE).entity(new ErrorMessage(Response.Status.NOT_FOUND.getStatusCode(),"User not found !")).build();
+
+            UserDescriptor userDescriptor = (UserDescriptor) userUpdateRequest;
+            this.conf.getInventoryDataManager().updateUser(userUpdateRequest);
+
+            return Response.noContent().build();
+
+        } catch (Exception e){
+            e.printStackTrace();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).type(MediaType.APPLICATION_JSON_TYPE).entity(new ErrorMessage(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(),"Internal Server Error !")).build();
+        }
+    }
+
+    @RolesAllowed("ADMIN")
+    @DELETE
+    @Path("/{username}")
+    @Timed
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value="Delete a Single User")
+    public Response deleteDevice(@Context ContainerRequestContext req,
+                                 @PathParam("username") String username) {
+
+        try {
+
+            logger.info("Deleting User with id: {}", username);
+
+            //Check the request
+            if(username == null)
+                return Response.status(Response.Status.BAD_REQUEST).type(MediaType.APPLICATION_JSON_TYPE).entity(new ErrorMessage(Response.Status.BAD_REQUEST.getStatusCode(),"Invalid Username Provided !")).build();
+
+            //Check if the device is available or not
+            if(!this.conf.getInventoryDataManager().getUser(username).isPresent())
+                return Response.status(Response.Status.NOT_FOUND).type(MediaType.APPLICATION_JSON_TYPE).entity(new ErrorMessage(Response.Status.NOT_FOUND.getStatusCode(),"User Not Found !")).build();
+
+            //Delete the location
+            this.conf.getInventoryDataManager().deleteUser(username);
+
+            return Response.noContent().build();
+
+        } catch (Exception e){
+            e.printStackTrace();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).type(MediaType.APPLICATION_JSON_TYPE).entity(new ErrorMessage(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(),"Internal Server Error !")).build();
         }
     }
 
