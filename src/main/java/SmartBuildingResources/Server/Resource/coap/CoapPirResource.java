05800d2c62f97ce22a18b848d194a398bcea7389
zanna <zanna1000@gmail.com>
2020-12-10 17:58:04 +0100

    Starting CoAP Resouces

---
+++ src/main/java/SmartBuildingResources/Server/Resource/coap/CoapPirResource.java
@@ -0,0 +1,9 @@
+package SmartBuildingResources.Server.Resource.coap;
+
+import org.eclipse.californium.core.CoapResource;
+
+public class CoapPirResource extends CoapResource {
+    public CoapPirResource(String name) {
+        super(name);
+    }
+}
