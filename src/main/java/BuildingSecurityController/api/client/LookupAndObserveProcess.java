74ea8859b1e03c702fa968a04d4af4888fde7399
Tobia <tobia.poppi@virgilio.it>
2020-12-12 19:17:19 +0100

    with errors to be fixed

--- src/main/java/BuildingSecurityController/api/client/LookupAndObserveProcess.java
+++ src/main/java/BuildingSecurityController/api/client/LookupAndObserveProcess.java
@@ -1,18 +1,19 @@
 package BuildingSecurityController.api.client;
 
-import org.eclipse.californium.core.CoapClient;
-import org.eclipse.californium.core.CoapObserveRelation;
-import org.eclipse.californium.core.CoapResponse;
-import org.eclipse.californium.core.Utils;
+import org.eclipse.californium.core.*;
 import org.eclipse.californium.core.coap.CoAP.Code;
+import org.eclipse.californium.core.coap.LinkFormat;
+import org.eclipse.californium.core.coap.MediaTypeRegistry;
 import org.eclipse.californium.core.coap.Request;
 import org.eclipse.californium.elements.exception.ConnectorException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import utils.CoreInterfaces;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class LookupAndObserveProcess {
 
@@ -20,6 +21,10 @@ public class LookupAndObserveProcess {
 
     private static final String COAP_ENDPOINT = "coap://127.0.0.1:<porta>/rd-lookup/res";
     //private static final String COAP_ENDPOINT = "coap://<futuro_ip_del_raspberry>:<porta>/rd-lookup/res";
+
+    private static final String OBSERVABLE_CORE_ATTRIBUTE = "obs";
+    private static final String INTERFACE_CORE_ATTRIBUTE = "if";
+    private static final String WELL_KNOWN_CORE_URI = "/.well-known/core";
 
     private static List<String> targetObservableResList = null;
     private static Map<String, CoapObserveRelation> observingRelationMap = null;
@@ -42,7 +47,37 @@ public class LookupAndObserveProcess {
         try{
             coapResponse = coapClient.advanced(request);
 
-            logger.info("Response Pretty Print: \n{}", Utils.prettyPrint(coapResponse));
+
+            //la risposta non deve essere nulla
+            if(coapResponse != null){
+
+                logger.info("Response Pretty Print: \n{}", Utils.prettyPrint(coapResponse));
+
+                //la risposta non deve essere di un media type che non sia CORE LINK FORMAT
+                if (coapResponse.getOptions().getContentFormat() == MediaTypeRegistry.APPLICATION_LINK_FORMAT){
+
+                    Set<WebLink> links = LinkFormat.parse(coapResponse.getResponseText());
+
+                    links.forEach(webLink -> {
+
+
+                        //I DUE IF SONO SBAGLIATI, DA SISTEMARE
+                        //TODO
+                        //if(webLink.getURI() != null && !webLink.getURI().equals(WELL_KNOWN_CORE_URI) && webLink.getAttributes() != null && webLink.getAttributes().getAttributeValues(INTERFACE_CORE_ATTRIBUTE).get(0).equals(CoreInterfaces.CORE_A.getValue())){
+
+                            //if (webLink.getAttributes().containsAttribute(OBSERVABLE_CORE_ATTRIBUTE) &&
+                                    webLink.getAttributes().containsAttribute(INTERFACE_CORE_ATTRIBUTE) &&
+                                    (webLink.getAttributes().getAttributeValues(INTERFACE_CORE_ATTRIBUTE).contains(CoreInterfaces.CORE_S.getValue())))
+
+                        }
+
+                    });
+
+                }
+
+            }
+
+
 
             String text = coapResponse.getResponseText();
             logger.info("Payload: {}", text);
